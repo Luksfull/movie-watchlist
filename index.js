f4e8d1962d5d8ce11dbe5be02be6c2a53e6ccfc8
Luksfull <105737001+Luksfull@users.noreply.github.com>
2024-02-14 19:33:05 +0200

    Fixed indentation in the .js files

--- index.js
+++ index.js
@@ -18,51 +18,53 @@ let filmList = document.getElementById('film-list')
 searchBtn.addEventListener('click', searchFilms)
 
 function searchFilms(e) {
-    e.preventDefault()
-    const apiKey = '1d5bc7d3'
+
+  e.preventDefault()
+  const apiKey = '1d5bc7d3'
     
-    if(searchName.value){
+  if(searchName.value){    
+    document.getElementById('intro').style.display = 'none'
         
-        document.getElementById('intro').style.display = 'none'
-        
-        fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchName.value}`)
+    fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchName.value}`)
+      .then(res => res.json())
+      .then(data => {        
+        data.Search.forEach(movie => {				
+        	fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`)				
             .then(res => res.json())
-            .then(data => {        
-                data.Search.forEach(movie => {   
-                        fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`)
-                            .then(res => res.json())
-                            .then(data => {
-                                filmList.innerHTML += `
-                                    <div class='result'>
-                                        <img src='${movie.Poster}' width=100px height=150px>
-                                        <div class='movie-details'>
-                                        
-                                            <div class='title-rating'>
-                                                <span class='movie-title'>${movie.Title}</span>
-                                                <i class="fa-solid fa-star fa-xs" id="imdb-star" style="color: #fec654;"></i>
-                                                <span class='imdb-rating'>${data.imdbRating}</span>
-                                            </div>
-                                            
-                                            <div class='runtime-genre'>
-                                                <span>${data.Runtime}</span>
-                                                <span>${data.Genre}</span>
-                                                <button class='add-btn' data-add='${movie.imdbID}'><i class="fa-solid fa-circle-plus fa-xs" style="color: #ffffff;"></i>Watchlist</button>
-                                            </div>
-                                            
-                                            <div class='plot'>
-                                                ${data.Plot}
-                                            </div>
-                                            
-                                        </div>
-                                    </div>
-                                    `
-                            })
+            .then(data => {
+							
+              filmList.innerHTML += 
+								`
+                <div class='result'>
+                    <img src='${movie.Poster}' width=100px height=150px>
+                    <div class='movie-details'>
+                    
+                        <div class='title-rating'>
+                            <span class='movie-title'>${movie.Title}</span>
+                            <i class="fa-solid fa-star fa-xs" id="imdb-star" style="color: #fec654;"></i>
+                            <span class='imdb-rating'>${data.imdbRating}</span>
+                        </div>
+                        
+                        <div class='runtime-genre'>
+                            <span>${data.Runtime}</span>
+                            <span>${data.Genre}</span>
+                            <button class='add-btn' data-add='${movie.imdbID}'><i class="fa-solid fa-circle-plus fa-xs" style="color: #ffffff;"></i>Watchlist</button>
+                        </div>
+                        
+                        <div class='plot'>
+                            ${data.Plot}
+                        </div>
+                        
+                    </div>
+                </div>
+                `
+            })
                             
-                            filmList.innerHTML = ""
+            filmList.innerHTML = ""
                             
-                })
         })
-    }
+      })
+  }
 }
 
 filmList.addEventListener('click', e => {
